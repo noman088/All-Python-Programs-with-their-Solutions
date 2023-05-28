e8576e895b56026fe3179560933c7c834d5b645f
Noman <mdnoman70499@gmail.com>
2023-05-28 22:43:35 +0530

    solutionOF8

--- 12 More on Loops/06_FirstNPrimeNumbers.py
+++ 12 More on Loops/06_FirstNPrimeNumbers.py
@@ -1,13 +1,18 @@
 # 6. Write a python script to print first N prime numbers
-num = int(input("Enter a Number"))
-a = 1
-i = 2
-while a <= num:
-    while i<a:
-        if(a%i==0):
-            a+=1
+a = int(input("Enter a number: "))
+while a:
+    i = 2
+    j = 2
+    while True:
+        flag = 0
+        for j in range(2, i):
+            if i % j == 0:
+                flag = 1
+        if flag == 0:
+            print(i, end=" ")
+            a -= 1
+        if a == 0:
             break
-        i+=1
-    else:
-        print(a)
-    
+        i += 1
+
+print()
